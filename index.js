e5eb849e45d4d6eaf7f9a309bc910f7bf97b277b
Kyle <kylennardsy@hotmail.com>
2020-02-03 21:39:18 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+
+const PORT = process.env.PORT || 5000; // Heroku environment or port 5000 for development
+app.listen(PORT);
