d9ca148d94ba6c7965ed91fb82ea8a404e594f67
Kyle <kylennardsy@hotmail.com>
2020-02-25 22:14:12 -0500

    minor change

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -37,7 +37,7 @@ class Header extends Component {
             to={this.props.auth ? "/surveys" : "/"}
             className="left brand-logo"
           >
-            Emaily
+            Emaily Dashboard
           </Link>
           <ul className="right">{this.renderContent()}</ul>
         </div>
